7b19d8a8310c2874d6a34cbea2fd6041bb92b9b1
MESUT <MESUT@DESKTOP-2GUDG4T>
2019-06-17 17:16:46 +0300

    Son yürüğü yere göre idle ve baktığı yöne göre (Sağ ve sol için) saldırı animasyonu ayarlandı

--- ShooterBoi/Assets/Scripts/PlayerMovement.cs
+++ ShooterBoi/Assets/Scripts/PlayerMovement.cs
@@ -18,6 +18,10 @@ public class PlayerMovement : MonoBehaviour
     private Vector2 xMovVector;
     private Animator anim2d;
 
+    private Vector2 lastMoveDir;
+
+    bool lookRight = true;
+
     private void Awake()
     {
         rb2d = GetComponent<Rigidbody2D>();
@@ -39,18 +43,37 @@ public class PlayerMovement : MonoBehaviour
         }
 
 
-            if (xMov < 0)
+        if (xMov < 0)
+        {
+            //transform.rotation = Quaternion.Euler(transform.rotation.x, -180, transform.rotation.z);
+            if (lookRight)
             {
-                transform.rotation = Quaternion.Euler(transform.rotation.x, -180, transform.rotation.z);
-                anim2d.SetFloat("HorizontalMovement", xMov);
+                FlipFace();
             }
-            else if (xMov >= 0)
+        }
+        else if (xMov > 0)
+        {
+            //transform.rotation = Quaternion.Euler(transform.rotation.x, 0, transform.rotation.z);
+            if (!lookRight)
             {
-                transform.rotation = Quaternion.Euler(transform.rotation.x, 0, transform.rotation.z);
-                anim2d.SetFloat("HorizontalMovement", xMov);
+                FlipFace();
             }
+        }
 
-            anim2d.SetFloat("VerticalMovement", yMov);
+        anim2d.SetFloat("HorizontalMovement", xMov);
+        anim2d.SetFloat("VerticalMovement", yMov);
+        anim2d.SetFloat("MovementMagnitude", new Vector2(xMov, yMov).magnitude);
+
+        bool isIdle = xMov == 0 && yMov == 0;
+        if(isIdle)
+        {
+            anim2d.SetFloat("IdleRight", lastMoveDir.x);
+            anim2d.SetFloat("IdleUP", lastMoveDir.y);
+        }
+        else
+        {
+            lastMoveDir = new Vector2(xMov, yMov);
+        }
 
         if (Input.GetMouseButtonDown(0))
         {
@@ -59,12 +82,19 @@ public class PlayerMovement : MonoBehaviour
             attackType.AnimateAttack(anim2d);
         }
     }
-
     private void FixedUpdate()
     {
-            xMovVector = new Vector2(xMov * speed * Time.fixedDeltaTime, yMov * speed * Time.fixedDeltaTime);
-            rb2d.velocity = xMovVector;
+        xMovVector = new Vector2(xMov * speed * Time.fixedDeltaTime, yMov * speed * Time.fixedDeltaTime);
+        rb2d.velocity = xMovVector;
     }
+
+    void FlipFace()
+    {
+        Debug.Log("Flipped Face");
+        lookRight = !lookRight;
+        transform.Rotate(Vector3.up * 180);
+    }
+
 
     public void AttackStateChange()
     {
